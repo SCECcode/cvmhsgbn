af1bbae93ddfe5afd5215199ccfd2bff5c9f55b5
Mei-Hui Su <mei@MeiPro.local>
2022-04-29 23:24:17 -0700

    add cvmhbn

--- test_validation/run_ucvm_validate.sh
+++ test_validation/run_ucvm_validate.sh
@@ -1,9 +1,6 @@
 ##
 ##  validate using UCVM 
 ##
-rm -rf validate_ucvm_bad.txt
-rm -rf cvmhsgbn_ucvm_rerun.c
-make cvmhsgbn_ucvm_rerun
 
 if [ ! -f ./validate_api_good.txt  ]; then 
   echo "need to run run_api_validate.sh first!!!"
@@ -15,8 +12,10 @@ if [ "x${UCVM_INSTALL_PATH}" != "x" ] ; then
   source $SCRIPT_DIR/../conf/ucvm_env.sh
   echo " === UCVM query by cvmh depth ==="
   ./cvmhsgbn_ucvm_validate -c ${SCRIPT_DIR}/../conf/ucvm.conf -f ./validate_api_good.txt
-  echo " === UCVM query by ucvm depth & ucvm_query with cvmh depth for the failed cases ==="
+  echo " === UCVM query by ucvm depth ==="
   ./cvmhsgbn_ucvm_rerun -c ${SCRIPT_DIR}/../conf/ucvm.conf -f ./validate_ucvm_bad.txt
+  echo " === ucvm_query with cvmh depth  ==="
+  ./cvmhsgbn_ucvm_retry -c ${SCRIPT_DIR}/../conf/ucvm.conf -f ./ucvm_rerun_bad.txt
   else
     echo "need to have UCVM_INSTALL_PATH set!!!"
 fi
